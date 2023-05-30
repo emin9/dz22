ffddf66c1baf14b7b977de46d7b651726d62374c
emin9 <129058356+emin9@users.noreply.github.com>
2023-05-30 23:34:42 +0300

    Add files via upload

---
+++ dz22/main.js
@@ -0,0 +1,14 @@
+const arr1 = [101, 202, 303, 404, 505]
+const arr2 =  [606, 707, 808, 909]
+const superArr = [...arr1,...arr2]
+console.log(superArr);
+console.log(Math.min(...superArr))
+
+
+const obj = {
+    width : 300,
+    height : 550
+}
+
+
+
